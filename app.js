45d54e3598956145fc38d0904c754579be64860f
ink1982 <ink1982@outlook.com>
2023-10-14 19:48:02 +0300

    Змінні, значення, арефметичні оператори

---
+++ app.js
@@ -0,0 +1,13 @@
+const width = 10
+const height = 5
+const space = width * height
+const newWidth = width - 4
+const newWidth2 = width + 4
+const division = newWidth / newWidth2
+const volume = 2 ** 3 // 2 * 2 * 2
+console.log(volume)
+
+// Рядки
+const city = 'Київ'
+const street = 'Дарницька'
+console.log(city + ', ' + street + ' ' + 5)
