0702da21caec38ceac4597ff8aa271d8b6c623c8
ink1982 <ink1982@outlook.com>
2023-10-16 13:25:30 +0300

    Вирази if else

--- app.js
+++ app.js
@@ -1,28 +1,12 @@
-// Перетворення типів
-const age = '18'
-console.log(Number(age) + 5)
-console.log(+age + 5)
-console.log(age - 3)
+// if else
+const money = 100
+const canBuy = money > 50
 
-const userName = 'Peter'
-console.log(Number(userName) + 5)
-
-console.log(typeof NaN)
-
-console.log(String(4) + 7)
-console.log(Boolean(1))
-console.log(Boolean(0))
-console.log(Boolean(100))
-console.log(Boolean(-100))
-console.log(Boolean('gjhgjh'))
-console.log(Boolean(''))
-
-const a = 2 + '10'
-console.log(a - 10)
-// =========== False із інших типів ==============
-
-console.log(Boolean(0))
-console.log(Boolean(''))
-console.log(Boolean(undefined))
-console.log(Boolean(null))
-console.log(Boolean(Number('sx')))
+if (canBuy) {
+	console.log('Може купити наш продукт')
+} else if (money > 55) {
+	console.log('Куплено mini продукт')
+} else {
+	console.log('Не вистачає балансу')
+}
+console.log('Підсумок')
