bd609cf47b19a0c3c842a178a7277e54eac42558
ink1982 <ink1982@outlook.com>
2023-10-17 21:34:24 +0300

    Таска-2. РОзміщення депозиту

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 // if else
-const money = 100
-const canBuy = money > 50
+const money = 100;
+const canBuy = money > 50;
 
 if (canBuy) {
-	console.log('Може купити наш продукт')
+	console.log('Може купити наш продукт');
 } else if (money > 55) {
-	console.log('Куплено mini продукт')
+	console.log('Куплено mini продукт');
 } else {
-	console.log('Не вистачає балансу')
+	console.log('Не вистачає балансу');
 }
-console.log('Підсумок')
+console.log('Підсумок');
--- task.js
+++ task.js
@@ -10,9 +10,31 @@
 	- Скільки ви за неї попросите.
 */
 
-const payRateUSD = 80
-const projectHours = 40
-const availableHours = (11 - 2) * 5
+const payRateUSD = 80;
+const projectHours = 40;
+const availableHours = (11 - 2) * 5;
 
-console.log('Чи зможу я працювати? ' + (availableHours > projectHours))
-console.log('Вартість робіт: ' + projectHours * payRateUSD + '$')
+console.log('Чи зможу я працювати? ' + (availableHours > projectHours));
+console.log('Вартість робіт: ' + projectHours * payRateUSD + '$');
+
+/*
+Василь поклав 12 000$ на вклад 7% річних з капіталізацією 1 раз в місяць.
+Вивести в консоль, чи зможе він придбати будинок за 13 500$ через 2 роки
+після того як зніме вій влад. І залишок після покупки.
+
+Підсумок = Сума * (1 + Ставка вв місяць не в %)^ строк в місяцях
+*/
+
+const deposit = 12000;
+const rate = 0.07;
+const depositLength = 24;
+const houseCost = 13500;
+
+const res = deposit * (1 + rate / 12) ** 24;
+if (res > houseCost) {
+	console.log(
+		`Ми наскладали: ${res}. Можемо купити. Залишок ${res - houseCost}`
+	);
+} else {
+	console.log('Ми наскладали: ${res}. Купити не зможемо (((');
+}
