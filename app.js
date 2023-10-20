af7db2304cb9297401e77f59f60462ea65d98809
ink1982 <ink1982@outlook.com>
2023-10-20 19:33:35 +0300

    Тернарні оператори

--- app.js
+++ app.js
@@ -1,50 +1,18 @@
-//switch
-const role = 'admin';
+//тернарні оператори
+const bmwX3Price = 100000;
+const fordFocusPrice = 10000;
+const budget = 200000;
 
-if (role === 'manager') {
-	console.log('Менеджер');
-} else if (role === 'admin') {
-	console.log('Адмін');
-} else if (role === 'SEO') {
-	console.log('СЕО');
-} else {
-	console.log('Ми вас не знаємо!');
-}
+// let message;
+// if (budget > bmwX3Price) {
+// 	message = 'BMW';
+// } else {
+// 	message = 'Велосипед';
+// }
 
-switch (role) {
-	case 'manager':
-		console.log('Менеджер');
-		break;
-	case 'admin':
-		console.log('Адмін');
-		break;
-	case 'SEO':
-		console.log('СЕО');
-		break;
-	default:
-		console.log('Ми вас не знаємо!');
-}
+// let message = budget > bmwX3Price ? 'BMW' :
 
-switch (role) {
-	case 'manager':
-	case 'admin':
-		console.log('Не керівник');
-		break;
-	case 'SEO':
-		console.log('Керівник');
-		break;
-	default:
-		console.log('Ми вас не знаємо!');
-}
+let message =
+	budget > bmwX3Price ? 'BMW' : budget > fordFocusPrice ? 'Ford' : 'Велосипед';
 
-const num = 1;
-switch (true) {
-	case num > 0:
-		console.log('Додатне число');
-		break;
-	case num < 0:
-		console.log("Від'ємне число");
-		break;
-	default:
-		console.log('Нуль');
-}
+console.log(`Я хочу купити ${message}`);
