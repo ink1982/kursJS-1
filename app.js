cc54c8a1ef9df383dfa2f624e895d8476e9a1e07
ink1982 <ink1982@outlook.com>
2023-10-20 08:15:25 +0300

    Оператор swich

--- app.js
+++ app.js
@@ -1,13 +1,50 @@
-// оператори рівності
-const secretNumber = '7';
+//switch
+const role = 'admin';
 
-if (Number(secretNumber) === 7) {
-	console.log('Вгадав жорстко');
+if (role === 'manager') {
+	console.log('Менеджер');
+} else if (role === 'admin') {
+	console.log('Адмін');
+} else if (role === 'SEO') {
+	console.log('СЕО');
 } else {
-	console.log('Вгадав не жорстко');
+	console.log('Ми вас не знаємо!');
 }
 
-const q = Number(prompt('Введіть число'));
-if (q === 7) {
-	console.log('Вгадав не жорстко');
+switch (role) {
+	case 'manager':
+		console.log('Менеджер');
+		break;
+	case 'admin':
+		console.log('Адмін');
+		break;
+	case 'SEO':
+		console.log('СЕО');
+		break;
+	default:
+		console.log('Ми вас не знаємо!');
+}
+
+switch (role) {
+	case 'manager':
+	case 'admin':
+		console.log('Не керівник');
+		break;
+	case 'SEO':
+		console.log('Керівник');
+		break;
+	default:
+		console.log('Ми вас не знаємо!');
+}
+
+const num = 1;
+switch (true) {
+	case num > 0:
+		console.log('Додатне число');
+		break;
+	case num < 0:
+		console.log("Від'ємне число");
+		break;
+	default:
+		console.log('Нуль');
 }
