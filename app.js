bab3cd75f975ea79829a81d884c6a32d6435ae8c
ink1982 <ink1982@outlook.com>
2023-10-21 12:43:15 +0300

    Функції у функціях

--- app.js
+++ app.js
@@ -1,11 +1,27 @@
-//умови в середині функцій
-function canAccessWebsite(age) {
-	if (age < 18) {
-		return false;
-	}
-	return true;
-}
-console.log(canAccessWebsite(18));
+//функції в середині функцій
 
-const canAccessWebsite2 = age => (age < 18 ? 'Ні' : 'Так');
-console.log(canAccessWebsite2(18));
+const KG_IN_USD = 7;
+const KM_IN_USD = 5;
+
+function calculateW(present) {
+	return present * KG_IN_USD;
+}
+
+function calculateKm(distance) {
+	return distance * KM_IN_USD;
+}
+
+function getExchangePrice(present1, present2, distance) {
+	const price1 = calculateW(present1);
+	console.log(price1);
+
+	const price2 = calculateW(present2);
+	console.log(price2);
+
+	const distancePrice = calculateKm(distance);
+	console.log(distancePrice);
+
+	return price1 + price2 + distancePrice;
+}
+
+console.log(getExchangePrice(1, 2, 10));
