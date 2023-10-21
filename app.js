b51bc627f13c7957f7491b1d14dc8cf9745e8c8c
ink1982 <ink1982@outlook.com>
2023-10-21 09:35:53 +0300

    Null merge operator

--- app.js
+++ app.js
@@ -1,19 +1,5 @@
-// Оператори з іншими типами
+// Null merge operator
 
-console.log('Василь' || 'Олег');
-console.log(false || 'Олег');
-console.log('Василь' || false);
-console.log(false || false);
-
-console.log('Василь' && 'Олег');
-console.log(false && 'Олег');
-console.log('Василь' && false);
-console.log(false && false);
-
-let Igor;
-const userName = 'Igor' || 'Петро';
-console.log(userName);
-
-const isAdmin = true;
-const fileName = isAdmin && 'file.mp4';
-console.log(fileName);
+const age = 0;
+console.log(age || 18);
+console.log(age ?? 18);
