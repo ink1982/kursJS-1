c38e03eaa1565b676a45a59a15156e5479370ba4
ink1982 <ink1982@outlook.com>
2023-10-21 10:15:15 +0300

    Знайомство з функціями

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-//
+// Знайомство з функціями
+function logName(name, surname) {
+	console.log(`Моє ім\'я ${name} ${surname}`);
+}
+
+logName('John', 'Dou');
+
+function countDepositSum(depositInUSD, month, rate) {
+	return depositInUSD * (1 + rate / 12) ** month;
+}
+
+const example1 = countDepositSum(1000, 24, 0.12);
+console.log(example1);
