52991e989f87826f2d56b1b859eaa0efaa2c0f70
ink1982 <ink1982@outlook.com>
2023-10-25 20:13:33 +0300

    деструктуризація

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-//
+// деструктуризація
+const userData = ['Антон', 18, 'Львів'];
+
+function getData() {
+	return ['Антон', 18, 'Львів'];
+}
+
+// const userName = userData[0];
+// const age = userData[1];
+// const city = userData[2];
+
+const [userName, age, city] = getData();
+
+console.log(userName, age, city);
