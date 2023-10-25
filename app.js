4bdf5c31739b30b91c5c3f5cb144589bc552e2f7
ink1982 <ink1982@outlook.com>
2023-10-25 18:42:11 +0300

    gеретворення з рядка в масив і навпаки (split, join)

--- app.js
+++ app.js
@@ -1,31 +1,10 @@
-// Slice, splice, concat, reverse
+// Перетворення з рядка в масив і навпаки
+// split, join
+
 const roles = ['user', 'admin', 'manager', 'superuser'];
 
-const res = roles.slice(2);
-console.log(roles);
+const url = 'auth/user/login';
+const res = url.split('/');
 console.log(res);
 
-const res1 = roles.slice(2, 3);
-console.log(res);
-
-const res2 = roles.slice(-2);
-console.log(res);
-
-const res3 = roles.slice(1, -1);
-console.log(res3);
-// ===================================
-
-// const res4 = roles.splice(2, 1);
-// console.log(res4);
-
-// const res5 = roles.splice(-1);
-// console.log(res5);
-
-// ==================================
-
-const res6 = roles.reverse();
-console.log(roles);
-
-const newRoles = ['sysadmin', 'developer'];
-const res7 = roles.concat(newRoles);
-console.log(res7);
+console.log(roles.join('-'));
