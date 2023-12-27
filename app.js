618039c57cb9e3d02220d5ec5b3f816071913577
ink1982 <ink1982@outlook.com>
2023-12-27 13:30:22 +0200

    метод масиву map

--- app.js
+++ app.js
@@ -1,38 +1,18 @@
-// Функції вищого порядку, Callback, повернення функції
+// map
+const transactionInUSD = [10, -7, 50, -10, 100];
 
-// function add(a, b) {
-// 	return a + b;
-// }
+const transactionInGRN = [];
+for (const transaction of transactionInUSD) {
+	transactionInGRN.push(transaction * 41.0);
+}
 
-// function subtract(a, b) {
-// 	return a - b;
-// }
+// console.log(transactionInUSD);
+// console.log(transactionInGRN);
 
-// function power(a, b) {
-// 	return a ** b;
-// }
-
-// Функція вищого порядку
-// function calculate(a, b, fn) {
-// 	console.log(fn.name);
-// 	const res = fn(a, b);
-// 	return res;
-// }
-
-// let res = calculate(3, 5, add);
-// console.log(res);
-
-// res = calculate(3, 5, subtract);
-// console.log(res);
-
-// res = calculate(3, 5, power);
-// forEach
-const score = [5, 10, 0, 15];
-
-// for (const [i, el] of score.entries()) {
-// 	console.log(`Раунд ${i + 1}: ${el}`);
-// }
-
-score.forEach((el, i) => {
-	console.log(`Раунд ${i + 1}: ${el}`);
+const transactionInGRN2 = transactionInUSD.map((transaction, i) => {
+	console.log(i);
+	return transaction * 41;
 });
+
+console.log(transactionInUSD);
+console.log(transactionInGRN2);
